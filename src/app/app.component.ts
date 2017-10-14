10beefe9b449987ec7d5f372158700ea0996f504
Kristopher Parrillo <kkparrillo@gmail.com>
2017-10-14 09:46:41 -0700

    adds hero-detail component and heroService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,32 @@
 import { Component } from '@angular/core';
 import { Hero } from './shared/hero.model';
+import { HEROES } from './mock-heroes';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
 
 export class AppComponent {
+  
+  constructor(private heroService: HeroService) {
+  }
+
   title = 'Tour of Heroes';
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+  selectedHero: Hero;
+  heroes = HEROES;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   };
-  heroes: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }  
-  ];
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
 }
 
 
